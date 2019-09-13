eba6fbc53c31b066fbd47465a1a3efba4c1da926
Juanjo Torres <juanjotorres90@gmail.com>
2019-09-13 10:18:40 +0200

    Firebase set up

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "YOUR_CREDENTIALS_HERE",
-    authDomain: "YOUR_CREDENTIALS_HERE",
-    databaseURL: "YOUR_CREDENTIALS_HERE",
-    projectId: "YOUR_CREDENTIALS_HERE",
-    storageBucket: "YOUR_CREDENTIALS_HERE",
-    messagingSenderId: "YOUR_CREDENTIALS_HERE"
+    apiKey: "AIzaSyAOgAl6SR1TpeUW-Q-5sidNHRJBhswSIt0",
+    authDomain: "ionic-firebase-starter-a-e5ab8.firebaseapp.com",
+    databaseURL: "https://ionic-firebase-starter-a-e5ab8.firebaseio.com",
+    projectId: "ionic-firebase-starter-a-e5ab8",
+    storageBucket: "",
+    messagingSenderId: "713975656058",
+    appId: "1:713975656058:web:5bf664bf580f73f63715c3"
   }
 };
 
